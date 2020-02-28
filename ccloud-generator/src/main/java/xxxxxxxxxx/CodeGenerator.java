c986677cabbcf7385755d5249f614f1e75fd0a6f
suhui <495092562@qq.com>
2020-02-28 16:00:24 +0800

    更新代码生成器配置

--- ccloud-generator/src/main/java/xxxxxxxxxx/CodeGenerator.java
+++ ccloud-generator/src/main/java/xxxxxxxxxx/CodeGenerator.java
@@ -28,19 +28,13 @@ class CodeGenerator {
     private static String PASSWORD;
 
     /**
-     * 读取控制台内容
+     * mac系统需自己配置输出路径 "/dev/codeGen/ccloud"
      */
-    private static String scanner() throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入" + "表名" + "：");
-        if (scanner.hasNext()) {
-            String ipt = scanner.next();
-            if (StringUtils.isNotEmpty(ipt)) {
-                return ipt;
-            }
-        }
-        throw new MybatisPlusException("请输入正确的" + "表名" + "！");
-    }
+    private static final String OUT_DIR = "D://codeGen//ccloud-core";
+    /**
+     * 包路径配置
+     */
+    private static final String parentPath= "com.ccloud.main";
 
     public static void main(String[] args) throws IOException {
         dbProperties = new Properties();
@@ -50,45 +44,48 @@ class CodeGenerator {
         USER_NAME = dbProperties.getProperty("username");
         PASSWORD = dbProperties.getProperty("password");
 
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
 
-        AutoGenerator mpg = new AutoGenerator();// 代码生成器
-
-        GlobalConfig gc = new GlobalConfig();// 全局配置
-        String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/ccloud-core/src/main/java");
-        gc.setAuthor("Generator");
-        gc.setOpen(false);
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(OUT_DIR);
+        gc.setAuthor("ccloud");
+        gc.setOpen(true);
         gc.setFileOverride(true);
         gc.setIdType(IdType.AUTO);
-        gc.setBaseResultMap(true);//生成resultMap
-        gc.setBaseColumnList(true);//在xml中生成基础列
+        //生成mapper.xml中的resultMap
+        gc.setBaseResultMap(true);
+        //在mapper.xml中生成基础列
+        gc.setBaseColumnList(true);
         mpg.setGlobalConfig(gc);
 
-        DataSourceConfig dsc = new DataSourceConfig();// 数据源配置
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
         dsc.setUrl(url);
-        // dsc.setSchemaName("public");
         dsc.setDriverName(DRIVER_CLASS);
         dsc.setUsername(USER_NAME);
         dsc.setPassword(PASSWORD);
 
         mpg.setDataSource(dsc);
 
-        PackageConfig pc = new PackageConfig();// 包配置
-//        pc.setModuleName(scanner("模块名"));
-        pc.setParent("com.ccloud.main");
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+
+        pc.setParent(parentPath);
         mpg.setPackageInfo(pc);
 
+        // 自定义配置
         InjectionConfig cfg = new InjectionConfig() {
             @Override
             public void initMap() {
                 // to do nothing
             }
-        };// 自定义配置
-
-        String templatePath = "/templates/mapper.xml.ftl";// 如果模板引擎是 freemarker
+        };
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
         // 如果模板引擎是 velocity
         // String templatePath = "/templates/mapper.xml.vm";
-
         // 自定义输出配置
         List<FileOutConfig> focList = new ArrayList<>();
         // 自定义配置会被优先输出
@@ -96,10 +93,9 @@ class CodeGenerator {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名
-                return projectPath + "/src/main/resources/mapper/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+                return OUT_DIR + "/com/ccloud/main/mapper/xml/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
-
         cfg.setFileOutConfigList(focList);
         mpg.setCfg(cfg);
 
@@ -108,25 +104,25 @@ class CodeGenerator {
 
         // 配置自定义输出模板
         //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
-//        templateConfig.setEntity("abc.java");
-//        templateConfig.setService("...");
+        // templateConfig.setEntity("abc.java");
+        // templateConfig.setService("...");
         templateConfig.setController(null);
-
         templateConfig.setXml(null);
-        mpg.setTemplate(templateConfig);
+        //mpg.setTemplate(templateConfig);
 
-        // 策略配置
+        //策略配置
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setSuperEntityClass("com.ccloud.main.entity.BaseEntity");
         strategy.setEntityLombokModel(true);
-//        strategy.setRestControllerStyle(true);
-//        strategy.setSuperControllerClass("BaseController");
-        strategy.setInclude(scanner());
-//        strategy.setSuperEntityColumns("id");//sb
+        strategy.setRestControllerStyle(true);
+        //strategy.setSuperControllerClass("BaseController");
+        //strategy.setInclude(scanner());
+        //strategy.setSuperEntityColumns("id");//sb
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(pc.getModuleName() + "_");
+
         mpg.setStrategy(strategy);
         mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
