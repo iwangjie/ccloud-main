59d1cf5c5294bbdcce26cf0c66ecbfb1ea617fda
wangjie <wangchenmo2015@gmail.com>
2020-02-29 18:02:05 +0800

    跨域

--- ccloud-core/src/main/java/com/ccloud/main/config/filter/CrosFilterConfig.java
+++ ccloud-core/src/main/java/com/ccloud/main/config/filter/CrosFilterConfig.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 /**
  * 跨域配置
+ *
  * @author wangjie
  * @date 2020-02-21
  */
@@ -29,28 +30,12 @@ public class CrosFilterConfig implements Filter {
             throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
         HttpServletRequest req = (HttpServletRequest) request;
-        if (req.getMethod().equals("OPTIONS")) {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-            res.setHeader("Access-Control-Max-Age", "3600");
-            res.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-            res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "*");
+        res.setHeader("Access-Control-Max-Age", "3600");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
 
-        }
-
-
-//        if (!org.springframework.util.StringUtils.isEmpty(origin)) {
-//            //带cookie的时候，origin必须是全匹配，不能使用*
-//        }
-//        res.addHeader("Access-Control-Allow-Methods", "*");
-//        String headers = req.getHeader("Access-Control-Request-Headers");
-//        if (!org.springframework.util.StringUtils.isEmpty(headers)) {
-//            // 支持所有自定义头
-//            res.addHeader("Access-Control-Allow-Headers", headers);
-//        }
-//        res.addHeader("Access-Control-Max-Age", "3600");
-//        // enable cookie
-//        res.addHeader("Access-Control-Allow-Credentials", "true");
         chain.doFilter(request, response);
     }
 
