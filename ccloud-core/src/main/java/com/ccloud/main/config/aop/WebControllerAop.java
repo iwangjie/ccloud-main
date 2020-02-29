692e2d5b6cc9307c625f407dc16583f47ca41562
杨航 <yanghaha_@163.com>
2020-02-29 14:51:13 +0800

    master

--- ccloud-core/src/main/java/com/ccloud/main/config/aop/WebControllerAop.java
+++ ccloud-core/src/main/java/com/ccloud/main/config/aop/WebControllerAop.java
@@ -1,42 +1,11 @@
 package com.ccloud.main.config.aop;
 
-import com.ccloud.main.logic.BusinessRequestLogLogic;
-import com.ccloud.main.pojo.enumeration.CloudUtilEnum;
-import com.ccloud.main.util.CloudUtil;
-import com.fasterxml.jackson.databind.JsonNode;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 
 /**
  * Web切面统一处理日志
  *
  * @author yanghang
  */
-@Slf4j
-/*@Aspect
-@Component*/
 public class WebControllerAop  {
 
-    @Autowired
-    private BusinessRequestLogLogic businessRequestLogLogic;
-
-    @Pointcut("execution(public * com.ccloud.main.controller..*.*(..))")
-    public void webLog(){}
-
-    @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) {
-        JsonNode jsonNode = (JsonNode) CloudUtil.get(CloudUtilEnum.CURR_REQUEST_BODY);
-        JsonNode appId = jsonNode.get("appId");
-        JsonNode userId = jsonNode.get("userId");
-        log.info("userId:{}", jsonNode.get("appId"));
-
-        businessRequestLogLogic.addBusinessRequestLog(joinPoint);
-
-    }
 }
